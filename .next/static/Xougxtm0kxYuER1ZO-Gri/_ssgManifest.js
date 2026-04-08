45038cabc8b90165e6b4e48b140577f174955d64
Syeda Juveria Afreen <sja.affu765@gmail.com>
2026-04-08 14:04:36 +0300

    Add .gitignore and remove build artifacts from tracking

--- .next/static/Xougxtm0kxYuER1ZO-Gri/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fpaths\u002F[pathSlug]","\u002Fpaths\u002F[pathSlug]\u002F[topicSlug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- .next/static/chunks/app/layout-7434a5a9e3efd3d2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1743:()=>{},7755:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,3549,23)),Promise.resolve().then(n.t.bind(n,21,23)),Promise.resolve().then(n.t.bind(n,421,23)),Promise.resolve().then(n.t.bind(n,9695,23)),Promise.resolve().then(n.t.bind(n,1743,23))}},e=>{e.O(0,[72,549,726,441,826,358],()=>e(e.s=7755)),_N_E=e.O()}]);
